326dbc90093ec9de7ebbc324a195f4a2b74b40fa
VictoriaWoo <Victoria.woo23@ycdsbk12.ca>
2022-06-27 00:46:42 -0400

    completed

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,29 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
+  PImage imgCharacter;
+	PImage imgCone;
+  PImage imgCream;
+  PImage imgPlatformLeft;
+  PImage imgPlatformMid;
+  PImage imgPlatformRight;
+
+  int intObjectX = 280;
+  int intObjectY = 250;
+  int intObjectSpeed = 3;
+  
+  int intRandom = (int) random(width);
+  int intCreamX;
+  int intCreamY;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(560, 400);
   }
 
   /** 
@@ -16,21 +31,61 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+
+    imgCharacter = loadImage("Player_Character.png");
+    imgCharacter.resize(imgCharacter.width/2, imgCharacter.height/2);
+    imgCone = loadImage("Cone_Base.png");
+    imgCone.resize(imgCone.width * 2/3, imgCone.height * 2/3);
+    imgCream = loadImage("creamVanilla.png");
+    imgPlatformLeft = loadImage("cakeHalfAltLeft.png");
+    imgPlatformMid = loadImage("cakeHalfAltMid.png");
+    imgPlatformRight = loadImage("cakeHalfAltRight.png");
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    background(143, 224, 242);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  	//draw images to screen
+  	image(imgPlatformLeft, 0, height-imgPlatformLeft.height/2);
+    image(imgPlatformMid, 70, height-imgPlatformMid.height/2);
+    image(imgPlatformMid, 140, height-imgPlatformMid.height/2);
+    image(imgPlatformMid, 210, height-imgPlatformMid.height/2);
+    image(imgPlatformMid, 280, height-imgPlatformMid.height/2);
+    image(imgPlatformMid, 350, height-imgPlatformMid.height/2);
+    image(imgPlatformMid, 420, height-imgPlatformMid.height/2);
+    image(imgPlatformRight, 490, height-imgPlatformRight.height/2);
+    image(imgCharacter, intObjectX - imgCharacter.width/2, intObjectY);
+    image(imgCone, intObjectX - imgCone.width/2, intObjectY);
+  
+    PlayerMovement();
+    FallingBlocks();
   }
   
-  // define other methods down here.
+  public void PlayerMovement() {
+    if(keyPressed){
+      if(key == 'a'){
+        intObjectX -= intObjectSpeed;
+      }
+
+      else if(key == 'd'){
+        intObjectX += intObjectSpeed;
+      }
+    }
+  }
+
+  public void FallingBlocks() {
+    
+    image(imgCream, intRandom, intCreamY);
+    intCreamY += 1;
+  
+
+    if (intObjectX == intRandom && intObjectY == intCreamY) {
+      intCreamX = intObjectX;
+      intCreamY = intObjectY;
+    }
+  }
 }
